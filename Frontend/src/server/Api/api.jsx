a2e53f605cfef774d63b8e116dc6a53b2a372649
meet_rajani <meet.rajani@swiftrut.com>
2025-01-24 01:06:46 +0530

    add sum changes

--- Frontend/src/server/Api/api.jsx
+++ Frontend/src/server/Api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const URL = "https://instagram-clone-saf1.onrender.com";
-const URL = "http://localhost:5000";
+const URL = "https://instagram-clone-saf1.onrender.com";
+// const URL = "http://localhost:5000";
 
 axios.interceptors.request.use(
   (config) => {
